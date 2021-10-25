fbb6fb4d63433b2adcb398ebe6b13ef053e6980a
TravLord <Travisblord@gmail.com>
2021-10-25 16:29:06 -0600

    Adding Basic_HTML_1,Basic_HTML_2, Basic_JavaScript_1.js and Microchip picture source to remote repository
    Sent with all  tag and attributes applicable to assignment

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert ("Hello World! I am an alert box!")
+
+function My_First_Function() {
+			var string = "Kiss Me I'm Irish!";
+			var result = String.fontcolor("green");
+			document.GetElementById("Irish").innerHTML = 
+			result;
+}
